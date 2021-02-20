86aa7fc6eff53b686bdda795ef2cffebc4cb118c
Boro23 <404slimboysavage@gmail.com>
2021-02-20 15:20:25 +0530

    added-pagination

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,21 +1,50 @@
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
+
 import Planet from './Planet';
 
-const fetchPlanets = async () => {
-  const res = await fetch('http://swapi.dev/api/planets/');
+const fetchPlanets = async ({ queryKey }) => {
+  const [_key, { page }] = queryKey;
+
+  const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
   return res.json();
 };
 
 const Planets = () => {
-  const { data, status } = useQuery('Planets', fetchPlanets);
+  const [page, setPage] = useState(1);
 
-  console.log(data);
+  const { data, status } = useQuery(['Planets', { page }], fetchPlanets);
+
   return (
     <div>
       <h2>Planets</h2>
 
-      {status === 'loading' && <div>Loading Planets... Please be patient!</div>}
-      {status === 'error' && <div>Uh Oh! Error loading planets...</div>}
+      {status === 'success' && (
+        <>
+          <button
+            disabled={!data.next}
+            onClick={() => setPage((page) => page + 1)}
+          >
+            Next
+          </button>
+          Page - {page}
+          <button
+            disabled={!data.previous}
+            onClick={() => setPage((page) => page - 1)}
+          >
+            Previous
+          </button>
+        </>
+      )}
+
+      {status === 'loading' && (
+        <div style={{ marginTop: '20px' }}>
+          Loading Planets... Please be patient!
+        </div>
+      )}
+      {status === 'error' && (
+        <div style={{ marginTop: '20px' }}>Uh Oh! Error loading planets...</div>
+      )}
       {status === 'success' && (
         <div>
           {data.results.map((planet) => (
